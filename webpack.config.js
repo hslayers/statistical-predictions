9330fa7163273f02f30d8b58be450c809ec9e5e1
raitis <raitisbe@gmail.com>
2022-11-11 13:35:04 +0200

    build: Make more customized module federation config

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,50 @@ module.exports = withModuleFederationPlugin({
   },
 
   shared: {
-    ...shareAll({ singleton: true, strictVersion: false, requiredVersion: 'auto' }),
-  },
+    "@angular/core": {
+      singleton: true,
+      strictVersion: false,
+      requiredVersion: "auto",
+    },
+    "@angular/common": {
+      singleton: true,
+      strictVersion: false,
+      requiredVersion: "auto",
+    },
+    "@angular/forms": {
+      singleton: true,
+      strictVersion: false,
+      requiredVersion: "auto",
+    },
+    "@angular/common/http": {
+      singleton: true,
+      strictVersion: false,
+      requiredVersion: "auto",
+    },
+    "@angular/router": {
+      singleton: true,
+      strictVersion: false,
+      requiredVersion: "auto",
+    },
+    "@ngx-translate/core": { singleton: true, strictVersion: false },
+    "@angular/localize/init": {
+      singleton: true,
+      eager: true,
+    },
+    'ol': {singleton: true, requiredVersion: '^7.1.0'},
+    'ol-ext': {singleton: true, requiredVersion: '^4.0.3'},
+    'ol/layer': {
+      singleton: true,
+    },
+    'ol/source': {
+      singleton: true,
+    },
+    'hslayers-ng': {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: '^10.0.0',
+      version: '10.0.0'
+    },
+  }
 
-
-});
+})
